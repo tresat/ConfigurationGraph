e092783176d979392469ba13eb85967b1d604b68
Tom Tresansky (Dell-laptop) <tom.tresansky@gmail.com>
2018-01-22 13:10:26 -0500

    Stubbed Plugin, verified project imports into IDEA, ignored IDEA files.

---
+++ src/main/kotlin/ConfigurationGraphPlugin.kt
@@ -0,0 +1,10 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ConfigurationPlugin : Plugin<Project> {
+  override fun apply(project: Project) {
+    with(project) {
+      
+    }
+  }
+}
