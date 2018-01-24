897073641978886a6b4e8e1a5712a5b8d1ce53b4
Tom Tresansky (Dell-laptop) <tom.tresansky@gmail.com>
2018-01-24 13:35:53 -0500

    Working on getting plugin setup.

--- src/main/kotlin/ConfigurationGraphPlugin.kt
+++ src/main/kotlin/ConfigurationGraphPlugin.kt
@@ -1,10 +1,28 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class ConfigurationPlugin : Plugin<Project> {
-  override fun apply(project: Project) {
-    with(project) {
-      
+open class ConfigurationGraphPluginExtension {
+    companion object {
+        const val DEFAULT_OUTPUT_FILE_NAME = "configuration_graph.txt"
     }
-  }
+
+    var outputFileName = DEFAULT_OUTPUT_FILE_NAME
+}
+
+class ConfigurationGraphPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        with(project) {
+            extensions.create("configurationGraph", ConfigurationGraphPluginExtension::class.java)
+
+            afterEvaluate {
+                val config = extensions.findByType(ConfigurationGraphPluginExtension::class.java)
+                val outputFileName = config?.outputFileName ?: ConfigurationGraphPluginExtension.DEFAULT_OUTPUT_FILE_NAME
+
+                val outputFile = file(outputFileName).apply { createNewFile() }
+                configurations.forEach { c ->
+                    outputFile.writeText(c.name)
+                }
+            }
+        }
+    }
 }
---
+++ src/test/kotlin/ConfigurationGraphPluginTest.kt
@@ -0,0 +1,19 @@
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
+import org.junit.Test
+
+class ConfigurationGraphPluginTest {
+    lateinit var project: Project
+
+    @Before
+    fun setUp() {
+        project = ProjectBuilder.builder().build()
+        project.pluginManager.apply(ConfigurationGraphPlugin::class.java)
+    }
+
+    @Test
+    fun `empty test`() {
+        project.configurations
+    }
+}
