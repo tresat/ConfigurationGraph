28cef2743c67a8b85552666c9714aace0e7dce9f
Tom Tresansky (Dell-laptop) <tom.tresansky@gmail.com>
2018-02-14 13:36:57 -0500

    Set up spek tests with powermockito.

--- src/main/kotlin/com/tomtresansky/gradle/plugin/configurationreport/internal/ConfigurationDataExtractor.kt
+++ src/main/kotlin/com/tomtresansky/gradle/plugin/configurationreport/internal/ConfigurationDataExtractor.kt
@@ -1,15 +1,23 @@
 package com.tomtresansky.gradle.plugin.configurationreport.internal
 
+import com.google.common.annotations.VisibleForTesting
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 
 class ConfigurationDataExtractor {
     fun extractConfigurationData(project: Project): List<ConfigurationNode> {
-        val result: List<ConfigurationNode> = arrayListOf()
+        val configs: MutableList<Configuration> = project.configurations.toCollection(mutableListOf())
+        return extractConfigurationData(configs)
+    }
 
-        with(project) {
-            configurations.forEach { c ->
-                TODO()
-            }
+    @VisibleForTesting
+    internal fun extractConfigurationData(configurations: List<Configuration>): List<ConfigurationNode> {
+        val result: MutableList<ConfigurationNode> = arrayListOf()
+
+        configurations.forEach { c ->
+            val node = ConfigurationNode(c, mutableListOf())
+
+            result.add(node)
         }
 
         return result.sortedBy { n -> n.config.name }
--- src/test/kotlin/com/tomtresansky/gradle/plugin/configurationreport/internal/ConfigurationDataExtractorTest.kt
+++ src/test/kotlin/com/tomtresansky/gradle/plugin/configurationreport/internal/ConfigurationDataExtractorTest.kt
@@ -1,6 +1,8 @@
 package com.tomtresansky.gradle.plugin.configurationreport.internal
 
+import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ConfigurationContainer
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -15,20 +17,17 @@ import org.mockito.Mockito.*
 val mockLoanCalculator = mock(LoanCalculator::class.java)
 `when`(mockLoanCalculator.calculateAmount(3)).thenReturn(300.0)
  */
-object ConfigurationNodeTest: Spek({
-    given("a configuration node with no children named 'Test'") {
-        val mockParent = mock(Configuration::class.java)
-        val testName = "Test"
-        `when`(mockParent.name).thenReturn(testName)
-        val children = listOf<ConfigurationNode>()
-        val node = ConfigurationNode(mockParent, children)
+object ConfigurationDataExtractorTest: Spek({
+   given("a configuration data extractor") {
+       val extractor = ConfigurationDataExtractor()
 
-        on("toString") {
-            val string = node.toString()
+       on("extracting data from an empty list of configurations") {
+           val configs = emptyList<Configuration>()
+           val configGraph = extractor.extractConfigurationData(configs)
 
-            it("should properly describe an empty node") {
-                assertEquals("\tTest;", string)
-            }
-        }
-    }
+           it("should properly describe an empty graph") {
+               assertEquals(emptyList<ConfigurationNode>(), configGraph)
+           }
+       }
+   }
 })
